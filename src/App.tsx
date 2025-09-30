a2c69706db675e5c239e095544016c947310466e
leehyunjun97 <monsterstamp79@gmail.com>
2025-09-30 14:08:26 +0900

    ✨ supabase 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import { Navigate, Route, Routes } from "react-router";
-import Default from "./layouts/Default";
-import LoginSocial from "./pages/auth/LoginSocial";
-import BlogList from "./pages/blog/BlogList";
-import BlogDetail from "./pages/blog/BlogDetail";
-import Profile from "./pages/profile/Profile";
-import BlogCreate from "./pages/blog/BlogCreate";
-import ProfileSetup from "./pages/auth/ProfileSetup";
+import { Navigate, Route, Routes } from 'react-router';
+import Default from './layouts/Default';
+import LoginSocial from './pages/auth/LoginSocial';
+import BlogList from './pages/blog/BlogList';
+import BlogDetail from './pages/blog/BlogDetail';
+import Profile from './pages/profile/Profile';
+import BlogCreate from './pages/blog/BlogCreate';
+import ProfileSetup from './pages/auth/ProfileSetup';
+import { useEffect } from 'react';
+import supabase from './utils/supabase';
 
 export default function App() {
+  useEffect(() => {
+    async function getTodos() {
+      const { data: todos } = await supabase.from('todos').select();
+      if (!todos) {
+        console.log('todos 없음');
+        return;
+      }
+      if (todos.length > 1) {
+        console.log(todos);
+      }
+    }
+
+    getTodos();
+  }, []);
+
   return (
     <>
       <Routes>
         <Route element={<Default />}>
-          <Route index element={<Navigate to="/blog" />} />
-          <Route path="blog" element={<BlogList />} />
-          <Route path="blog/create" element={<BlogCreate />} />
-          <Route path="blog/:id" element={<BlogDetail />} />
-          <Route path="login" element={<LoginSocial />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="profile-setup" element={<ProfileSetup />} />
+          <Route index element={<Navigate to='/blog' />} />
+          <Route path='blog' element={<BlogList />} />
+          <Route path='blog/create' element={<BlogCreate />} />
+          <Route path='blog/:id' element={<BlogDetail />} />
+          <Route path='login' element={<LoginSocial />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='profile-setup' element={<ProfileSetup />} />
         </Route>
       </Routes>
     </>
